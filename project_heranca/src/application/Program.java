6616e18607345438f19cb00e095ca16a302ffd48
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-06-12 13:21:59 -0300

    Override

--- project_heranca/src/application/Program.java
+++ project_heranca/src/application/Program.java
@@ -7,29 +7,37 @@ import entities.SavingsAccount;
 public class Program {
 
 	public static void main(String[] args) {
-		Account acc = new Account(1001, "Alex", 0.0);
+		Account acc = new Account(1001, "Alex", 1000.0);
 		BusinessAccount bacc = new BusinessAccount(1002, "Maria", 0.0, 500.0);
 
 		// UPCASTING
 		Account acc1 = bacc;
 		Account acc2 = new BusinessAccount(1003, "Bob", 0.0, 200.0);
-		Account acc3 = new SavingsAccount(1004, "Anna", 0.0, 0.01);
+		Account acc3 = new SavingsAccount(1004, "Anna", 1000.0, 0.01);
 
 		// DOWNCASTING
 		BusinessAccount acc4 = (BusinessAccount) acc2;
 		acc4.loan(100.0);
 
+		acc.withdraw(200.0);
+		System.out.println(acc.getBalance());
+
+		acc3.withdraw(200.0);
+		System.out.println(acc3.getBalance());
+
 		if (acc3 instanceof BusinessAccount) {
 			BusinessAccount acc5 = (BusinessAccount) acc3;
 			acc5.loan(200.0);
-			System.out.println("Loan!");
 		}
 
 		if (acc3 instanceof SavingsAccount) {
 			SavingsAccount acc5 = (SavingsAccount) acc3;
 			acc5.updateBalance();
-			System.out.println("Update!");
 		}
+
+		Account acc6 = new BusinessAccount(1005, "Garen", 1000.0, 500.0);
+		acc6.withdraw(200.0);
+		System.out.println(acc6.getBalance());
 	}
 
 }
