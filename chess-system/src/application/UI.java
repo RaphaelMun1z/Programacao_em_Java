2cf5e5cebd198deca3d36d26d3657729ae492798
RaphaelMun1z <raphaelmunizvarela@hotmail.com>
2024-06-15 16:04:26 -0300

    Printing possible moves

--- chess-system/src/application/UI.java
+++ chess-system/src/application/UI.java
@@ -47,16 +47,31 @@ public class UI {
 		for (int l = 0; l < pieces.length; l++) {
 			System.out.print((8 - l) + " ");
 			for (int c = 0; c < pieces.length; c++) {
-				printPiece(pieces[l][c]);
+				printPiece(pieces[l][c], false);
 			}
 			System.out.println();
 		}
 		System.out.println("  a b c d e f g h");
 	}
 
-	private static void printPiece(ChessPiece piece) {
+	public static void printBoard(ChessPiece[][] pieces, boolean[][] possibleMoves) {
+		for (int l = 0; l < pieces.length; l++) {
+			System.out.print((8 - l) + " ");
+			for (int c = 0; c < pieces.length; c++) {
+				printPiece(pieces[l][c], possibleMoves[l][c]);
+			}
+			System.out.println();
+		}
+		System.out.println("  a b c d e f g h");
+	}
+
+	private static void printPiece(ChessPiece piece, boolean background) {
+		if (background) {
+			System.out.print(ANSI_BLUE_BACKGROUND);
+		}
+
 		if (piece == null) {
-			System.out.print("-");
+			System.out.print("-" + ANSI_RESET);
 		} else {
 			if (piece.getColor() == Color.WHITE) {
 				System.out.print(ANSI_WHITE + piece + ANSI_RESET);
